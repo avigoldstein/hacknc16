31f288272b3db796598e78a3b2aa94c787fc441e
aagoldst <avi.goldstein95@gmail.com>
2016-10-30 03:17:32 -0400

    updated errthang

--- linguafranca.js
+++ linguafranca.js
@@ -1,8 +1,24 @@
-var express = require('express')();
-var http = require('http').Server(express);
+var express = require('express');
+var expr = express();
+var http = require('http').Server(expr);
+var path = require('path');
+var io = require('socket.io')(http);
 
-express.get('/', function(req, res){
-  res.send('<h1>Hello World</h1>');
+expr.get('/', function(req, res){
+  res.sendFile(__dirname + '/linguafranca.html');
+});
+
+expr.use(express.static(path.join(__dirname + '/public')));
+
+io.on('connection', function(socket){
+  console.log("a user connected");
+  socket.on('chat message', function(msg){
+      console.log("trying to get values");
+      //console.log(msg.my_message);
+      //socket.emit("chat message", "hello");
+      socket.emit('chat message', msg.my_message);
+      socket.broadcast.emit('chat message', msg.message);
+  });
 });
 
 http.listen(3000, function(){
